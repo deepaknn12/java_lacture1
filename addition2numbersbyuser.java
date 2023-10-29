0d0c72d03369f293e1a0136265bcbc5adc4956bb
deepaknn12 <145982737+deepaknn12@users.noreply.github.com>
2023-10-30 00:25:57 +0530

    Add files via upload

---
+++ addition2numbersbyuser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class addition2numbersbyuser {
+    public static void main(String[] args) {
+        try (Scanner Scan = new Scanner(System.in)) {
+            int i = Scan.nextInt();
+            int j = Scan.nextInt();
+            int k = i + j;
+            System.out.println(k);
+        }
+    }
+}
