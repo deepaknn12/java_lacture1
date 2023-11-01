29249d0396e0d166e887d2216421ba33192a6ce7
deepaknn12 <145982737+deepaknn12@users.noreply.github.com>
2023-11-01 12:01:06 +0530

    Add files via upload

---
+++ largestofthreenumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class largestofthreenumber {
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int c = s.nextInt();
+            if (a >= b && a >= c) {
+                System.out.print(a);
+            } else {
+                if (b >= c && b >= a) {
+
+                    System.out.print(b);
+                } else {
+                System.out.print(c);
+                }
+            }
+        }
+    }
+
+}
---
+++ number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class number {
+    public static void main(String[]args){
+        try (Scanner s = new Scanner(System.in)) {
+        }
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+    
+}
