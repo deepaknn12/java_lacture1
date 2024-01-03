0c12860acc9712d932840f482951d7974ab9be2a
Deepak kumar <145982737+deepaknn12@users.noreply.github.com>
2024-01-03 13:02:38 +0530

    Update sumofnumber.java

--- sumofnumber.java
+++ sumofnumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class sumofnumber {
     public static void main(String[] args) {
-        try (Scanner s = new Scanner(System.in)) {
+        Scanner s = new Scanner(System.in)
             int n = s.nextInt();
             int i = 1;
           int sum=0;
@@ -14,4 +14,4 @@ public class sumofnumber {
         }
     }
 
-}
+
