030b8d56c2efe0fb4370903303fcd918d466ba69
Deepak kumar <145982737+deepaknn12@users.noreply.github.com>
2024-01-03 12:58:39 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class prime {
     public static void main(String[] args) {
-        try (Scanner s = new Scanner(System.in)) {
+        Scanner s = new Scanner(System.in);
             int n = s.nextInt();
             int d = 2;
             while (d <= n - 1) {
@@ -15,4 +15,3 @@ public class prime {
             System.out.println("prime");
         }
     }
-}
