6348f6496453826ba51b08950a94a81bd553a93c
Deepak kumar <145982737+deepaknn12@users.noreply.github.com>
2024-01-03 13:02:11 +0530

    Update sum_of_even.java

--- sum_of_even.java
+++ sum_of_even.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class sum_of_even {
 
     public static void main(String[] args) {
-	try(Scanner s = new Scanner(System.in)) {
+	Scanner s = new Scanner(System.in);
         int n=s.nextInt();
         int sum=0;
         int i=1;
@@ -17,5 +17,5 @@ System.out.println(sum);
     }
 	}
 
-}
+
 
